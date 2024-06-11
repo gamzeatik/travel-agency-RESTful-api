50292f7399654fccb83f496f5d32b41c6d88c073
gamzeatik <82980529+gamzeatik@users.noreply.github.com>
2024-06-11 18:12:41 +0300

    refactor: edited endpoint url

--- src/main/java/travelAgency/agency/app/TransferDestinationsController.java
+++ src/main/java/travelAgency/agency/app/TransferDestinationsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/transfer-destinations")
+@RequestMapping("/api/transfer-destinations")
 public class TransferDestinationsController {
     private final TransferDestinationsService transferDestinationsService;
 
